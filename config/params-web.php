dd92af04aabeb32a027c2bffdb0a6563ce74e37a
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-11-18 08:58:39 -0300

    Fix config default language. (#4)

--- config/params-web.php
+++ config/params-web.php
@@ -6,5 +6,5 @@ use yii\localeurls\UrlLanguageManager;
 
 return [
     'yii2.urlManager.class' => UrlLanguageManager::class,
-    'yii2.localeurls.languages' => ['en-*', 'es-*', 'fr-*', 'pt-*', 'ru-*'],
+    'yii2.localeurls.languages' => [],
 ];
