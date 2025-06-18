987c63531e57f37cb57f5eed5c80873bfa218398
Wilmer Arambula <terabytesoftw@gmail.com>
2025-06-18 08:28:34 -0400

    refactor: Replace `phpstan-mutant-killer-infection-runner` with `infection` in mutation workflow and scripts; remove installation script.

--- tests/install-mutant-killer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-if (version_compare(PHP_VERSION, '8.2.0', '>=')) {
-    passthru('composer require --dev phpstan/mutant-killer-infection-runner --no-scripts');
-}
